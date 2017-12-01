3b166d4b1f66febdd378bffa1d22ef0c0b1c6586
Eduardo Magalhaes <ed83isme@gmail.com>
2017-12-01 08:43:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
     @name
   end
   
-  def name = (name)
+  def name=(name)
     @name = name
   end
   
@@ -11,7 +11,7 @@ class Dog
     @breed
   end
   
-  def breed = (breed)
+  def breed=(breed)
     @breed = breed
   end
 end
